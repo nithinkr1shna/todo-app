02a61c8bf979d8b330dcf2d3f000808eedf8d419
nitkna <nithin.krishna@outlook.com>
2019-03-11 18:34:33 +0530

    destroys todos

--- todo-api/app/javascript/components/main.js
+++ todo-api/app/javascript/components/main.js
@@ -7,17 +7,33 @@ export default class Main extends Component {
     constructor(props){
 	super(props);
 	this.state ={
-	    isChecked : false
+	    todos: props.todos
 	}
+
+	this.removeDestroyedTodo = this.removeDestroyedTodo.bind(this);
     }
 
+    componentWillReceiveProps(nextProps){
+	this.setState({
+	    todos: nextProps.todos
+	})
+    }
+   
     
+    removeDestroyedTodo(destroyedTodo){
+	console.log("clicked")
+	this.setState({
+	    todos: this.state.todos.filter(function(todo){
+		return todo !== destroyedTodo
+	    })
+	});
+    }
 
    	render(){
 		return( 
-		    this.props.todos.length > 0 ? this.props.todos.map((todo)=>{
+		    this.state.todos.length > 0 ? this.state.todos.map((todo)=>{
 			return (
-			    <TodoItem todo = {todo}/>
+				<TodoItem todo = {todo} onRemoveDestroyedTodo = {this.removeDestroyedTodo}/>
 			
 			);
 			}): <div></div>
--- todo-api/app/javascript/components/todo_item.js
+++ todo-api/app/javascript/components/todo_item.js
@@ -9,6 +9,7 @@ export default class TodoItem extends Component {
 	    isdestroyed : false
 	}
 	this.handleChange = this.handleChange.bind(this);
+	this.onDestroy = this.onDestroy.bind(this);
     }
 
     handleChange(event){
@@ -27,6 +28,19 @@ export default class TodoItem extends Component {
 	    })
     }
 
+    onDestroy(event){
+	fetch(`api/v1/todos/${this.props.todo.id}`, {
+	    method: 'DELETE',
+	    headers: {
+			"Content-Type": "application/json"
+	    }
+	}).then((res)=> res.json())
+	    .then((data) => {
+		console.log(data)
+	    })
+	this.props.onRemoveDestroyedTodo(this.props.todo);
+    }
+
     render(){
 	var todo = this.props.todo;
 	console.log(this.props.todo.done)
@@ -38,9 +52,11 @@ export default class TodoItem extends Component {
 	    defaultChecked = {this.state.isChecked}
 	    onChange = {this.handleChange}
 		/>
-		<span className = "icon">icon </span>
+		<button className = "icon">Set Important </button>
 		<label className ="td-item">{todo.title}</label>
-		<button className ="destroy"></button>
+		<button className="destroy"
+	    onClick = {this.onDestroy}
+		></button>
 		</div>	
 		</li>				
 	);
